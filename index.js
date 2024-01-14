71db049b413cee438f4f91c10a8ac7f2a7e5dbf6
Mangesh Sali <97012992+mangeshsali@users.noreply.github.com>
2024-01-14 19:35:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,47 @@ hamburger.addEventListener("click", () => {
 iclose.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
+
+// Contact- form
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCRdGGt9Nr5Uc2yJA6uISnJLGnoUPlM2lM",
+  authDomain: "contactform-68901.firebaseapp.com",
+  databaseURL: "https://contactform-68901-default-rtdb.firebaseio.com",
+  projectId: "contactform-68901",
+  storageBucket: "contactform-68901.appspot.com",
+  messagingSenderId: "658065288439",
+  appId: "1:658065288439:web:807c738924f5d8c12cf7f2",
+  measurementId: "G-PBWRSZGB5P",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database().ref("contactForm");
+
+var form = document.getElementById("contactForm");
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log("je");
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  saveMessage(name, email, message);
+  var alert = document.querySelector(".alert");
+  alert.style.display = "block";
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 3000);
+  form.reset();
+}
+
+const saveMessage = (name, email, message) => {
+  var newDB = database.push();
+  newDB.set({
+    name: name,
+    email: email,
+    message: message,
+  });
+};
